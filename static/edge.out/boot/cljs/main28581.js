b785f014ee41ab0b90674aa1d2b44d7bed015b75
Jason Courcoux <jxc1206@googlemail.com>
2016-09-15 18:10:49 +0100

    Removing static folder

--- static/edge.out/boot/cljs/main28581.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.14 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main28581');
-goog.require('cljs.core');
-goog.require('edge.main');
-edge.main.init();
--- static/edge.out/reagent/interop.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.14 {:static-fns true, :optimize-constants true}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
